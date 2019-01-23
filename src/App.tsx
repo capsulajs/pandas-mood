147a622dd7db53b4f5177fba885052fd2da71632
Sergiu Padure <sergiu.padure@gmail.com>
2019-01-23 18:22:48 +0200

    added home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import logo from './logo.svg';
+import Home from './Home';
+import Publish from './Publish/Publish';
 
-const PageA = () => <div>Page A</div>;
-const PageB = () => <div>Page B</div>;
-const PageC = () => <div>Page C</div>;
+// TODO: use components after they will be created
+const Login = () => <div>Login</div>;
+const Dashboard = () => <div>Dashboard</div>;
 
 class App extends React.Component {
   public render() {
@@ -14,14 +16,16 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-          <Link to="/">Page A</Link>
-          <Link to="/publish">Page B</Link>
-          <Link to="/auth">Page C</Link>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/publish">Publish</Link>
+          <Link to="/dashboard">Dashboard</Link>
         </header>
         <div className="Content">
-          <Route exact path="/" component={PageA}/>
-          <Route path="/publish" component={PageB}/>
-          <Route path="/auth" component={PageC}/>
+          <Route path="/" component={Home}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/publish" component={Publish}/>
+          <Route path="/dashboard" component={Dashboard}/>
         </div>
       </div>
     </Router>;
