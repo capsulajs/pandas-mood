326841ea377f6b9574f1d95763cbde5593bb7c77
Igor Voitsekhivskyi <orhey@ukr.net>
2019-01-23 18:04:24 +0200

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,33 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+// @ts-ignore
+import app from 'firebase/app';
+import * as firebase from 'firebase';
+// import 'firebase/database';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { config } from './firebase.conf'
 
-declare const firebase: any;
+const email = 'arbib.michael@om2.com';
+const pass = 'mysuperpassword';
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+    firebase.auth().signInWithEmailAndPassword(email, pass).then((x) => console.log('ok', x)).catch((e) => console.log('ko', e));
+  }
+}
+
+export const FirebaseContext = React.createContext(null);
 
 ReactDOM.render(
-  <App />,
+  // @ts-ignore
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App/>
+  </FirebaseContext.Provider>,
   document.getElementById('root') as HTMLElement
 );
-
-firebase.initializeApp(config);
-
 
 
 registerServiceWorker();
