a69a2f49b71ae498d3e4983d7c24b30ca8e6aed6
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-01-23 16:54:44 +0200

    init

---
+++ src/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Dashboard.css';
+
+class Dashboard extends React.Component {
+  public render() {
+    return (
+      <div className="Dashboard">
+        chart should be here
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
