22803f05fd58f27b24981fa7a31cc5b9e2e5872e
Sergiu Padure <sergiu.padure@gmail.com>
2019-01-23 16:55:45 +0200

    added components

---
+++ src/Publish/Message.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Message.css';
+
+class Message extends React.Component {
+  public render() {
+    return (
+      <div className="Message">
+        Message
+      </div>
+    );
+  }
+}
+
+export default Message;
