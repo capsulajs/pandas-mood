9b8c6024d2425ddebeae5f30a91abbeb832e57e5
dmitriyslaym <dmitriyslaym@gmail.com>
2019-01-23 16:56:19 +0200

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,17 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { config } from './firebase.conf'
+
+declare const firebase: any;
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
+
+firebase.initializeApp(config);
+
+
+
 registerServiceWorker();
