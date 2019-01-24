fb8c195ab7a233aee6c20d6b2922353aeeef5f10
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-01-24 08:54:04 +0200

    finish

--- src/Dashboard/Chart/Chart.tsx
+++ src/Dashboard/Chart/Chart.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as moment from 'moment';
 import { Scatter } from 'react-chartjs-2';
 
-const mood = () => Math.round(Math.random() * 4);
 const dates = [
   '01/01/2019',
   '02/01/2019',
@@ -13,10 +12,12 @@ const dates = [
   '07/01/2019'
 ].map(i => new Date(i));
 
+const day = () => dates[Math.round(Math.random() * dates.length)];
+const mood = () => Math.round(Math.random() * 4);
 const data = {
   datasets: [{
     label: 'Mood',
-    data: Array.from(Array(50).keys()).map(() => ({ x: Math.round(Math.random() * dates.length), y: mood() })),
+    data: Array.from(Array(50).keys()).map(() => ({ x: day(), y: mood() })),
     borderColor: '#2196f3',
     backgroundColor: '#2196f3',
   }]
