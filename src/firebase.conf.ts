14bb47b13b2350ff62fc38cd5421edabfa477989
idokatz <idok.mail@gmail.com>
2019-01-24 13:43:34 +0200

    git ignore

--- src/firebase.conf.ts
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-export const config = {
-  apiKey: "AIzaSyD4Jo060p5wXVy8d12EjI1Bq7hFAaBwwO8",
-  authDomain: "panda-mood.firebaseapp.com",
-  databaseURL: "https://panda-mood.firebaseio.com",
-  projectId: "panda-mood",
-  storageBucket: "panda-mood.appspot.com",
-  messagingSenderId: "660205945236"
-};
