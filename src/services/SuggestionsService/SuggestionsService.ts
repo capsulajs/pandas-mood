23531afcad5c2f9f2f2f07978d334a0f9850fb65
idokatz <idok.mail@gmail.com>
2019-01-27 13:54:49 +0200

    auto complete as an observable of string[]

--- src/services/SuggestionsService/SuggestionsService.ts
+++ src/services/SuggestionsService/SuggestionsService.ts
@@ -5,29 +5,41 @@ import SuggestionsServiceDefinition, {
 } from '../api/SuggestionsService';
 
 import { tagsRef } from '../utils/firebase';
-import { from, Observable } from 'rxjs';
-import { filter } from "rxjs/operators";
+import { Observable, ReplaySubject } from 'rxjs';
+import { mergeMap, filter, multicast, refCount, scan } from 'rxjs/operators';
 
 export default class SuggestionsService implements SuggestionsServiceDefinition {
+  public tags$: Observable<string[]>;
+
   public analyze$(analyze: Observable<AnalyzeRequest>): Observable<AnalyzeResponse> {
     return Observable.create()
   }
 
   public autoComplete$(autoComplete: AutoCompleteRequest): Observable<AutoCompleteResponse> {
-    return Observable.create((observer: any) => {
-      tagsRef.on('value', (tags) => {
-        if (!tags) {
-          observer.error('Unable to retrieve tag list');
-        } else {
-          const historyTags = autoComplete.text === '' ? [] : tags.val();
+    if (!this.tags$) {
+      this.tags$ = Observable.create((observer: any) => {
+        tagsRef.on('value', (tagsDB) => {
+          if (!tagsDB) {
+            observer.error('Unable to retrieve tag list');
+          } else {
+            observer.next([].concat(tagsDB.val()));
+          }
+        });
 
-          // from(historyTags).pipe(
-          //   filter((val: string) => val.includes(autoComplete.text))
-          // );
-
-          observer.next(historyTags);
+        return () => {
+          tagsRef.off('value');
         }
-      });
-    });
+
+      }).pipe(
+        multicast(() => new ReplaySubject(1)),
+        refCount()
+      );
+    }
+
+    return this.tags$.pipe(
+      mergeMap(tag => tag),
+      filter((tag) => autoComplete.text !== '' && tag.includes(autoComplete.text)),
+      scan((acc: AutoCompleteResponse, val: string) => ({ suggestions: [...acc.suggestions, val] }), { suggestions: [] })
+    );
   }
 }
